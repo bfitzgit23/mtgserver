a07668eed27bb92c80dee4ffbadd867b1b978ab5
ben <dfitz@me.com>
2025-08-21 19:48:15 -0400

    Update QueueCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/QueueCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/QueueCommand.h
@@ -330,12 +330,12 @@ public:
 		skillMods.put(skillMod, value);
 	}
 
-	bool isWearingArmor(CreatureObject* creo) const {
+	/*bool isWearingArmor(CreatureObject* creo) const {
 		for (int i = 0; i < creo->getSlottedObjectsSize(); ++i) {
 			SceneObject* item = creo->getSlottedObject(i);
 			if (item != nullptr && item->isArmorObject())
 				return true;
-		}
+		}*/
 
 		return false;
 	}
