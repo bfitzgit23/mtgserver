77e910007dd13f4f3dc1e33f3d4761cb259791b6
ben <dfitz@me.com>
2025-08-21 23:56:16 -0400

    Create mission_level_choice.lua

---
+++ MMOCoreORB/bin/scripts/screenplays/tools/mission_level_choice.lua
@@ -0,0 +1,93 @@
+-- Allows players to choose a mission level range, regardless of their own CL/Group Level
+
+mission_level_choice = ScreenPlay:new {
+	numberOfActs = 1,
+
+	levels = {
+		{levelRange = "Reset Level Range", levelSelect = 0},
+		{levelRange = "Level 5-10", levelSelect = 1},
+		{levelRange = "Level 10-20", levelSelect = 5},
+		{levelRange = "Level 20-35", levelSelect = 10},
+		{levelRange = "Level 30-50", levelSelect = 15},
+		{levelRange = "Level 40-55", levelSelect = 20},
+		{levelRange = "Level 50-65", levelSelect = 25},
+		{levelRange = "Level 60-80", levelSelect = 30},
+		{levelRange = "Level 70-90", levelSelect = 35},
+		{levelRange = "Level 80-100", levelSelect = 40},
+		{levelRange = "Level 90-120", levelSelect = 45},
+		{levelRange = "Level 110-150", levelSelect = 50},
+		{levelRange = "Level 140-200", levelSelect = 60},
+		{levelRange = "Level 180-250", levelSelect = 70},
+		{levelRange = "Level 220-300", levelSelect = 80},
+		{levelRange = "Level 270-350", levelSelect = 90},
+		{levelRange = "Level 320-400", levelSelect = 100},
+		{levelRange = "Level 370-450", levelSelect = 110},
+		{levelRange = "Level 420-500", levelSelect = 120},
+	}
+}
+
+function mission_level_choice:start()
+
+end
+
+function mission_level_choice:openWindow(pPlayer)
+	if (pPlayer == nil) then
+		return
+	end
+
+	self:showLevels(pPlayer)
+end
+
+function mission_level_choice:showLevels(pPlayer)
+
+	local cancelPressed = (eventIndex == 1)
+
+	if (cancelPressed) then
+		return
+	end
+
+	local sui = SuiListBox.new("mission_level_choice", "levelSelection") -- calls levelSelection on SUI window event
+
+	sui.setTargetNetworkId(SceneObject(pPlayer):getObjectID())
+
+	sui.setTitle("Mission Level Selection")
+
+	local promptText = "Use this menu to select a mission level range to aim for.  After you have chosen, use the mission terminal to get a selection of missions (if any exist) within that range.  \n\nIf no missions are offered to you, it is because no missions in that level range exist for this planet.  You will need to choose another range.\n\nWhen you want to go back to the 'normal' offering of missions for your skill level/group level, just choose Reset Level Range."
+
+	sui.setPrompt(promptText)
+
+	for i = 1,  #self.levels, 1 do
+		sui.add(self.levels[i].levelRange, "")
+	end
+
+	sui.sendTo(pPlayer)
+end
+
+function  mission_level_choice:levelSelection(pPlayer, pSui, eventIndex, args)
+
+	local cancelPressed = (eventIndex == 1)
+
+	if (cancelPressed) then
+		return 
+	end
+
+	if (args == "-1") then
+		CreatureObject(pPlayer):sendSystemMessage("No level was selected...")
+		return
+	end
+
+	local selectedLevelIndex = tonumber(args)+1
+
+	local selectedLevel = self.levels[selectedLevelIndex].levelSelect
+	local selectedRange = self.levels[selectedLevelIndex].levelRange
+	
+	writeScreenPlayData(pPlayer, "mission_level_choice", "levelChoice", selectedLevel) 
+
+	if (tonumber(selectedLevel) == 0) then
+		CreatureObject(pPlayer):sendSystemMessage("You have selected: " .. selectedRange .. ".  You may now choose missions suited to your own skill/group level.")
+	else	
+		CreatureObject(pPlayer):sendSystemMessage("You have selected: " .. selectedRange .. ".  These missions would be considered suitable for a player or group of level " .. selectedLevel .. ".")
+
+	end
+
+end
