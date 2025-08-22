955c68a477a36f2670b629dc87e8ea6316ac8a8e
ben <dfitz@me.com>
2025-08-22 03:20:58 -0400

    Update mission_direction_choice.lua

--- MMOCoreORB/bin/scripts/screenplays/tools/mission_direction_choice.lua
+++ MMOCoreORB/bin/scripts/screenplays/tools/mission_direction_choice.lua
@@ -1,36 +1,38 @@
--- Allows players to choose a mission level range, regardless of their own CL/Group Level
+-- Allows players to choose the direction they want to take destroy missions
+--
+--							N 0/360
+--							|
+--					NW 45   |	NE 315
+--						  \ | /
+--						   \|/
+--			W 90  ----------+---------- E 270
+--						   /|\
+--						  / | \
+--					SW 135	|  SE 225
+--							|
+--							S 180
 
-mission_level_choice = ScreenPlay:new {
+mission_direction_choice = ScreenPlay:new {
 	numberOfActs = 1,
-
-	levels = {
-		{levelRange = "Reset Level Range", levelSelect = 0},
-		{levelRange = "Level 5-10", levelSelect = 1},
-		{levelRange = "Level 10-20", levelSelect = 5},
-		{levelRange = "Level 20-35", levelSelect = 10},
-		{levelRange = "Level 30-50", levelSelect = 15},
-		{levelRange = "Level 40-55", levelSelect = 20},
-		{levelRange = "Level 50-65", levelSelect = 25},
-		{levelRange = "Level 60-80", levelSelect = 30},
-		{levelRange = "Level 70-90", levelSelect = 35},
-		{levelRange = "Level 80-100", levelSelect = 40},
-		{levelRange = "Level 90-120", levelSelect = 45},
-		{levelRange = "Level 110-150", levelSelect = 50},
-		{levelRange = "Level 140-200", levelSelect = 60},
-		{levelRange = "Level 180-250", levelSelect = 70},
-		{levelRange = "Level 220-300", levelSelect = 80},
-		{levelRange = "Level 270-350", levelSelect = 90},
-		{levelRange = "Level 320-400", levelSelect = 100},
-		{levelRange = "Level 370-450", levelSelect = 110},
-		{levelRange = "Level 420-500", levelSelect = 120},
+	
+	directions = {
+		{dirDesc = "Reset Direction", dirSelect = 0},
+		{dirDesc = "North", dirSelect = 360},
+		{dirDesc = "North West", dirSelect = 45},
+		{dirDesc = "West", dirSelect = 90}, 
+		{dirDesc = "South West", dirSelect = 135}, 
+		{dirDesc = "South", dirSelect = 180}, 
+		{dirDesc = "South East", dirSelect = 225}, 
+		{dirDesc = "East", dirSelect = 270}, 
+		{dirDesc = "North East", dirSelect = 315}, 
 	}
 }
 
-function mission_level_choice:start()
+function mission_direction_choice:start()
 
 end
 
-function mission_level_choice:openWindow(pPlayer)
+function mission_direction_choice:openWindow(pPlayer)
 	if (pPlayer == nil) then
 		return
 	end
@@ -38,7 +40,7 @@ function mission_level_choice:openWindow(pPlayer)
 	self:showLevels(pPlayer)
 end
 
-function mission_level_choice:showLevels(pPlayer)
+function mission_direction_choice:showLevels(pPlayer)
 
 	local cancelPressed = (eventIndex == 1)
 
@@ -46,24 +48,24 @@ function mission_level_choice:showLevels(pPlayer)
 		return
 	end
 
-	local sui = SuiListBox.new("mission_level_choice", "levelSelection") -- calls levelSelection on SUI window event
+	local sui = SuiListBox.new("mission_direction_choice", "dirSelection") -- calls dirSelection on SUI window event
 
 	sui.setTargetNetworkId(SceneObject(pPlayer):getObjectID())
 
-	sui.setTitle("Mission Level Selection")
+	sui.setTitle("Mission Direction Selection")
 
-	local promptText = "Use this menu to select a mission level range to aim for.  After you have chosen, use the mission terminal to get a selection of missions (if any exist) within that range.  \n\nIf no missions are offered to you, it is because no missions in that level range exist for this planet.  You will need to choose another range.\n\nWhen you want to go back to the 'normal' offering of missions for your skill level/group level, just choose Reset Level Range."
+	local promptText = "Use this menu to select the direction in which you would like to take missions.  After you have chosen, use the mission terminal to get a selection of missions (if any exist) in that direction.  \n\nIf no missions are offered to you, it is because terrain is unsuitable for missions in that direction from your current location.  You will need to choose another direction.\n\nWhen you want to go back to the 'normal' offering of missions, just choose Reset Direction."
 
 	sui.setPrompt(promptText)
 
-	for i = 1,  #self.levels, 1 do
-		sui.add(self.levels[i].levelRange, "")
+	for i = 1,  #self.directions, 1 do
+		sui.add(self.directions[i].dirDesc, "")
 	end
 
 	sui.sendTo(pPlayer)
 end
 
-function  mission_level_choice:levelSelection(pPlayer, pSui, eventIndex, args)
+function  mission_direction_choice:dirSelection(pPlayer, pSui, eventIndex, args)
 
 	local cancelPressed = (eventIndex == 1)
 
@@ -72,22 +74,26 @@ function  mission_level_choice:levelSelection(pPlayer, pSui, eventIndex, args)
 	end
 
 	if (args == "-1") then
-		CreatureObject(pPlayer):sendSystemMessage("No level was selected...")
+		CreatureObject(pPlayer):sendSystemMessage("No direction was selected...")
 		return
 	end
 
-	local selectedLevelIndex = tonumber(args)+1
+	local selectedIndex = tonumber(args)+1
 
-	local selectedLevel = self.levels[selectedLevelIndex].levelSelect
-	local selectedRange = self.levels[selectedLevelIndex].levelRange
+	local selectedDir = tonumber(self.directions[selectedIndex].dirSelect)
+	local selectedDirDesc = self.directions[selectedIndex].dirDesc
 	
-	writeScreenPlayData(pPlayer, "mission_level_choice", "levelChoice", selectedLevel) 
+	writeScreenPlayData(pPlayer, "mission_direction_choice", "directionChoice", selectedDir) 
 
-	if (tonumber(selectedLevel) == 0) then
-		CreatureObject(pPlayer):sendSystemMessage("You have selected: " .. selectedRange .. ".  You may now choose missions suited to your own skill/group level.")
+	-- Debugging output
+	--CreatureObject(pPlayer):sendSystemMessage("DEBUG: Selected Index: " .. selectedIndex)
+	--CreatureObject(pPlayer):sendSystemMessage("DEBUG: Selected Direction: " .. selectedDir)
+	--CreatureObject(pPlayer):sendSystemMessage("DEBUG: Selected Description: " .. selectedDirDesc)
+
+	if (selectedDir == 0) then
+		CreatureObject(pPlayer):sendSystemMessage("Mission direction has been reset to normal randomization.")
 	else	
-		CreatureObject(pPlayer):sendSystemMessage("You have selected: " .. selectedRange .. ".  These missions would be considered suitable for a player or group of level " .. selectedLevel .. ".")
-
+		CreatureObject(pPlayer):sendSystemMessage("You have selected to take mission in the " .. selectedDirDesc .. " direction. This choice will remain active until you choose to change or reset it.")
 	end
 
 end
