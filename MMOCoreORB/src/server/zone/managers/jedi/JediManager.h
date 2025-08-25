5e70140186db06d37d5cf6a4b38c9cfe41656d03
ben <dfitz@me.com>
2025-08-25 19:37:07 -0400

    Update JediManager.h

--- MMOCoreORB/src/server/zone/managers/jedi/JediManager.h
+++ MMOCoreORB/src/server/zone/managers/jedi/JediManager.h
@@ -6,10 +6,10 @@
 #define JEDIMANAGER_H_
 
 #include "server/zone/objects/creature/CreatureObject.h"
-#include "server/zone/objects/scene/SceneObject.h"  // needed for SceneObject* in useItem()
+#include "server/zone/objects/scene/SceneObject.h"   // for SceneObject* in useItem()
+#include "engine/lua/Lua.h"                           // use the real Lua type
 
-// Minimal forward declarations used by function signatures
-class Lua;
+// Minimal forward declaration used by function signatures
 class Skill;
 
 namespace server {
@@ -22,146 +22,69 @@ namespace jedi {
  */
 class JediManager : public Singleton<JediManager>, public Logger, public Object, public ReadWriteLock {
 private:
-	/**
-	 * The Jedi progression type currently configured.
-	 */
+	// The Jedi progression type currently configured.
 	AtomicInteger jediProgressionType;
 
-	/**
-	 * The name of the jedi manager class in Lua.
-	 */
+	// The name of the jedi manager class in Lua.
 	String jediManagerName;
 
 	AtomicBoolean loaded{false};
 
-	/**
-	 * Setup Lua global values.
-	 * @param luaEngine the lua instance.
-	 */
+	// Setup Lua global values.
 	static void setupLuaValues(Lua* luaEngine);
 
 public:
-
 	enum {
-		NOJEDIPROGRESSION,         // Jedi progression not available.
-		HOLOGRINDJEDIPROGRESSION,  // Jedi progression through the hologrind system, i.e. master five random professions.
-		VILLAGEJEDIPROGRESSION,    // Jedi progression through the village system.
-		CUSTOMJEDIPROGRESSION      // Custom defined jedi progression system.
+		NOJEDIPROGRESSION,        // Jedi progression not available.
+		HOLOGRINDJEDIPROGRESSION, // Master five random professions.
+		VILLAGEJEDIPROGRESSION,   // Village system.
+		CUSTOMJEDIPROGRESSION     // Custom defined jedi progression system.
 	};
 
 	enum {
 		ITEMHOLOCRON,         // Item type holocron.
 		ITEMWAYPOINTDATAPAD,  // Item type waypoint datapad.
-		ITEMTHEATERDATAPAD    // ITEMTHEATERDATAPAD
+		ITEMTHEATERDATAPAD    // Item type theater datapad.
 	};
 
-	/**
-	 * Constructor for the Jedi Manager.
-	 */
 	JediManager();
-
-	/**
-	 * Destructor for the Jedi Manager.
-	 */
 	~JediManager();
 
-	/**
-	 * Load the Jedi Manager configuration.
-	 * @param luaEngine the lua engine to use for loading the configuration.
-	 */
+	// Load the Jedi Manager configuration.
 	void loadConfiguration(Lua* luaEngine);
 
-	/**
-	 * On player created.
-	 * @param creature the creature/player that was created.
-	 */
+	// Lifecycle hooks.
 	void onPlayerCreated(CreatureObject* creature);
-
-	/**
-	 * On player logged in.
-	 * @param creature the creature/player that logged in.
-	 */
 	void onPlayerLoggedIn(CreatureObject* creature);
-
-	/**
-	 * On player logged out.
-	 * @param creature the creature/player that logged out.
-	 */
 	void onPlayerLoggedOut(CreatureObject* creature);
-
-	/**
-	 * On player skill revoked.
-	 * @param creature the creature/player that revoked a skill
-	 * @param skill the skill that was revoked
-	 */
 	void onSkillRevoked(CreatureObject* creature, Skill* skill);
 
-	/**
-	 * Check force status command.
-	 * Calls the checkForceStatusCommand in the lua manager.
-	 * @param creature the creature that performed the command.
-	 */
+	// Commands.
 	void checkForceStatusCommand(CreatureObject* creature);
 
-	/**
-	 * Get the name of the currently active lua jedi manager.
-	 * @return the name of the currently active lua jedi manager.
-	 */
+	// Accessors.
 	const String& getJediManagerName();
-
-	/**
-	 * Get the value of the currently active jedi progression type.
-	 * @return the value of the currently active jedi progression type.
-	 */
 	int getJediProgressionType();
-
-	/**
-	 * Set the name of the currently active lua jedi manager.
-	 * @param name the name of the currently active lua jedi manager.
-	 */
 	void setJediManagerName(const String& name);
 
-	/**
-	 * Handle usage of any item related to the jedi progression.
-	 * @param item pointer to the item object.
-	 * @param itemType the type of item.
-	 * @param creature the creature that used the item.
-	 */
+	// Items related to jedi progression.
 	void useItem(SceneObject* item, const int itemType, CreatureObject* creature);
 
-	/**
-	 * Check for force skill prerequisites
-	 * @param creature the creature object.
-	 * @param skillName the name of the skill to check the prerequisite for
-	 */
+	// Skill gates.
 	bool canLearnSkill(CreatureObject* creature, const String& skillName);
-
-	/**
-	 * Check to ensure force skill prerequisites are maintained
-	 * @param creature the creature object.
-	 * @param skillName the name of the skill to be surrendered
-	 */
 	bool canSurrenderSkill(CreatureObject* creature, const String& skillName);
 
-	/**
-	 * Decides what to do next pending learning an FS tree.
-	 * Calls the onFSTreeCompleted in the lua manager.
-	 * @param creature the creature object.
-	 * @param branch String of the branch name.
-	 */
+	// FS tree completion.
 	void onFSTreeCompleted(CreatureObject* creature, const String& branch);
 
-	/**
-	 * Login-time fixer to raise existing Jedi to the configured baseline if needed.
-	 * Implemented in JediManager.cpp.
-	 */
+	// Login-time fixer to raise existing Jedi to the configured baseline if needed.
 	void applyBaselineIfNeeded(CreatureObject* creature);
 };
 
-}
-}
-}
-}
+}  // namespace jedi
+}  // namespace managers
+}  // namespace zone
+}  // namespace server
 
 using namespace server::zone::managers::jedi;
 
