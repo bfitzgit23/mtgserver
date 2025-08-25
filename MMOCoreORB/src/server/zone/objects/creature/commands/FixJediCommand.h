adab3bd0590f996e22b6d2d7d333555bad903ddb
ben <dfitz@me.com>
2025-08-24 23:47:06 -0400

    Create FixJediCommand.h

---
+++ MMOCoreORB/src/server/zone/objects/creature/commands/FixJediCommand.h
@@ -0,0 +1,24 @@
+/*
+    Copyright <SWGEmu>
+    See file COPYING for copying conditions.
+*/
+
+#ifndef FIXJEDICOMMAND_H_
+#define FIXJEDICOMMAND_H_
+
+#include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/ZoneProcessServer.h"
+#include "server/zone/objects/scene/commands/QueueCommand.h"
+
+class FixJediCommand : public QueueCommand {
+public:
+    FixJediCommand(const String& name, ZoneProcessServer* server)
+        : QueueCommand(name, server) {
+    }
+
+    int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const override;
+};
+
+#endif /* FIXJEDICOMMAND_H_ */
