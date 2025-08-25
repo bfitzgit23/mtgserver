cf4d9cbc20adf3fa4b49916fd394d5613f5ccc8b
ben <dfitz@me.com>
2025-08-25 04:01:56 -0400

    Update JediManager.h

--- MMOCoreORB/src/server/zone/managers/jedi/JediManager.h
+++ MMOCoreORB/src/server/zone/managers/jedi/JediManager.h
@@ -16,7 +16,7 @@ namespace server {
 namespace zone {
 namespace objects {
 namespace scene {
-class SceneObject; // <-- correct namespace for SceneObject
+class SceneObject; // correct namespace for SceneObject
 }
 }
 }
@@ -51,114 +51,40 @@ private:
     static void setupLuaValues(Lua* luaEngine);
 
 public:
-
     enum {
         NOJEDIPROGRESSION,         // Jedi progression not available.
-        HOLOGRINDJEDIPROGRESSION,  // Jedi progression through the hologrind system, i.e. master five random professions.
-        VILLAGEJEDIPROGRESSION,    // Jedi progression through the village system.
-        CUSTOMJEDIPROGRESSION      // Custom defined jedi progression system.
+        HOLOGRINDJEDIPROGRESSION,  // Master five random professions.
+        VILLAGEJEDIPROGRESSION,    // Village system.
+        CUSTOMJEDIPROGRESSION      // Custom progression.
     };
 
     enum {
-        ITEMHOLOCRON,         // Item type holocron.
-        ITEMWAYPOINTDATAPAD,  // Item type waypoint datapad.
-        ITEMTHEATERDATAPAD    // ITEMTHEATERDATAPAD
+        ITEMHOLOCRON,
+        ITEMWAYPOINTDATAPAD,
+        ITEMTHEATERDATAPAD
     };
 
-    /**
-     * Constructor for the Jedi Manager.
-     */
     JediManager();
-
-    /**
-     * Destructor for the Jedi Manager.
-     */
     ~JediManager();
 
-    /**
-     * Load the Jedi Manager configuration.
-     * @param luaEngine the lua engine to use for loading the configuration.
-     */
     void loadConfiguration(Lua* luaEngine);
 
-    /**
-     * On player created.
-     * @param creature the creature/player that was created.
-     */
     void onPlayerCreated(CreatureObject* creature);
-
-    /**
-     * On player logged in.
-     * @param creature the creature/player that logged in.
-     */
     void onPlayerLoggedIn(CreatureObject* creature);
-
-    /**
-     * On player logged out.
-     * @param creature the creature/player that logged out.
-     */
     void onPlayerLoggedOut(CreatureObject* creature);
-
-    /**
-     * On player skill revoked.
-     * @param creature the creature/player that revoked a skill
-     * @param skill the skill that was revoked
-     */
     void onSkillRevoked(CreatureObject* creature, Skill* skill);
 
-    /**
-     * Check force status command.
-     * Calls the checkForceStatusCommand in the lua manager.
-     * @param creature the creature that performed the command.
-     */
     void checkForceStatusCommand(CreatureObject* creature);
 
-    /**
-     * Get the name of the currently active lua jedi manager.
-     * @return the name of the currently active lua jedi manager.
-     */
     const String& getJediManagerName();
-
-    /**
-     * Get the value of the currently active jedi progression type.
-     * @return the value of the currently active jedi progression type.
-     */
     int getJediProgressionType();
-
-    /**
-     * Set the name of the currently active lua jedi manager.
-     * @param name the name of the currently active lua jedi manager.
-     */
     void setJediManagerName(const String& name);
 
-    /**
-     * Handle usage of any item related to the jedi progression.
-     * @param item pointer to the item object.
-     * @param itemType the type of item.
-     * @param creature the creature that used the item.
-     */
     void useItem(server::zone::objects::scene::SceneObject* item, const int itemType, CreatureObject* creature);
 
-    /**
-     * Check for force skill prerequisites
-     * @param creature the creature object.
-     * @param skillName the name of the skill to check the prerequisite for
-     */
     bool canLearnSkill(CreatureObject* creature, const String& skillName);
-
-    /**
-     * Check to ensure force skill prerequisites are maintained
-     * @param creature the creature object.
-     * @param skillName the name of the skill to be surrendered
-     */
     bool canSurrenderSkill(CreatureObject* creature, const String& skillName);
 
-    /**
-     * Decides what to do next pending learning an FS tree.
-     * Calls the onFSTreeCompleted in the lua manager.
-     * @param creature the creature object.
-     * @param branch String of the branch name.
-     */
     void onFSTreeCompleted(CreatureObject* creature, const String& branch);
 
     /**
@@ -168,11 +94,9 @@ public:
     void applyBaselineIfNeeded(CreatureObject* creature);
 };
 
-}
-}
-}
-}
-
-using namespace server::zone::managers::jedi;
+}  // namespace jedi
+}  // namespace managers
+}  // namespace zone
+}  // namespace server
 
 #endif /* JEDIMANAGER_H_ */
