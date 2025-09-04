9011f766cad02b439e570a7129fa833c97b7b522
ben <dfitz@me.com>
2025-09-04 02:13:01 -0400

    Update RobeObjectImplementation.cpp

--- MMOCoreORB/src/server/zone/objects/tangible/wearables/RobeObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/wearables/RobeObjectImplementation.cpp
@@ -1,8 +1,13 @@
-
 #include "server/zone/objects/tangible/wearables/RobeObject.h"
 #include "templates/SharedTangibleObjectTemplate.h"
+#include <cstdlib> // for std::getenv
 
 String RobeObjectImplementation::getSkillRequired() const {
+	// Environment toggle: export SWGR_DISABLE_WEARABLE_SKILL_REQS=1
+	if (const char* env = std::getenv("SWGR_DISABLE_WEARABLE_SKILL_REQS")) {
+		if (*env && *env != '0') return "";
+	}
+
 	auto tanoTemp = dynamic_cast<const SharedTangibleObjectTemplate*>(templateObject.get());
 	fatal(tanoTemp, "RobeObject must have a TANO template");
 
