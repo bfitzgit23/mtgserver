45991be91928fbe53062f04e846668c665f48142
ben <dfitz@me.com>
2025-09-04 12:28:41 -0400

    Update PlayerCreationManager.cpp

--- MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
+++ MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
@@ -524,7 +524,7 @@ bool PlayerCreationManager::createCharacter(ClientCreateCharacterCallback* callb
 
                             Time timeVal(sec);
 
-                            if (timeVal.miliDifference() < 3600000) {
+                            if (timeVal.miliDifference() < 0000000) {
                                 ErrorMessage* errMsg = new ErrorMessage("Create Error", "You are only permitted to create one character per hour. Repeat attempts prior to 1 hour elapsing will reset the timer.", 0x0);
                                 client->sendMessage(errMsg);
 
@@ -541,7 +541,7 @@ bool PlayerCreationManager::createCharacter(ClientCreateCharacterCallback* callb
                     if (lastCreatedCharacter.containsKey(accID)) {
                         Time lastCreatedTime = lastCreatedCharacter.get(accID);
 
-                        if (lastCreatedTime.miliDifference() < 3600000) {
+                        if (lastCreatedTime.miliDifference() < 0000000) {
                             ErrorMessage* errMsg = new ErrorMessage("Create Error", "You are only permitted to create one character per hour. Repeat attempts prior to 1 hour elapsing will reset the timer.", 0x0);
                             client->sendMessage(errMsg);
 
